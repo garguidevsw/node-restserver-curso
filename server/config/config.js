c68c4e89349faae2ded2a6c85a1c0d51e73acf9f
garguidevsw <garguidevsw@gmail.com>
2018-07-29 15:00:19 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // PUERTO
 //====================================
 process.env.PORT = process.env.PORT || 3000;
+
+//====================================
+// ENTORNO
+//====================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//====================================
+// BASE DE DATOS
+//====================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:ml4b-c4f3@ds259351.mlab.com:59351/cafe';
+}
+
+process.env.URLDB = urlDB;
