799a7b253eeceaba8a26809114ee9c5939c13cf2
garguidevsw <garguidevsw@gmail.com>
2018-07-29 15:25:47 -0500

    Variable de entorno Mongo en Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:ml4b-c4f3@ds259351.mlab.com:59351/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
